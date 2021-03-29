5083c04cf5519b0aa364b980a8d1a04e2182b799
Kazpenn <kazpenn2020@gmail.com>
2021-03-29 11:58:59 -0600

    Galleries display in progress.

--- app/src/main/java/edu/cnm/deepdive/gallery/controller/GalleryFragment.java
+++ app/src/main/java/edu/cnm/deepdive/gallery/controller/GalleryFragment.java
@@ -20,12 +20,13 @@ import edu.cnm.deepdive.gallery.NavGraphDirections;
 import edu.cnm.deepdive.gallery.NavGraphDirections.OpenUploadProperties;
 import edu.cnm.deepdive.gallery.R;
 import edu.cnm.deepdive.gallery.adapter.GalleryAdapter;
+import edu.cnm.deepdive.gallery.adapter.GalleryAdapter.OnGalleryClickHelper;
 import edu.cnm.deepdive.gallery.databinding.FragmentGalleryBinding;
 import edu.cnm.deepdive.gallery.model.Image;
 import edu.cnm.deepdive.gallery.viewmodel.MainViewModel;
 import java.util.List;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements OnGalleryClickHelper {
 
   private static final int PICK_IMAGE_REQUEST = 1023;
   private FragmentGalleryBinding binding;
@@ -78,7 +79,7 @@ public class GalleryFragment extends Fragment {
     int span = (int) Math.floor(context.getResources().getDisplayMetrics().widthPixels / context.getResources().getDimension(R.dimen.gallery_item_width));
     GridLayoutManager layoutManager = new GridLayoutManager(context, span);
     binding.galleryView.setLayoutManager(layoutManager);
-    adapter = new GalleryAdapter(context);
+//    adapter = new GalleryAdapter(context, galleries);
     binding.galleryView.setAdapter(adapter);
     binding.addImage.setOnClickListener((v) -> pickImage());
     return binding.getRoot();
@@ -101,16 +102,21 @@ public class GalleryFragment extends Fragment {
   }
 
   private void updateGallery(Image image) {
-    List<Image> images = adapter.getImages();
-    if(image != null && !images.contains(image)) {
-      images.add(0, image);
-      adapter.notifyItemInserted(0);
-    }
+//    List<Image> images = adapter.getImages();
+//    if(image != null && !images.contains(image)) {
+//      images.add(0, image);
+//      adapter.notifyItemInserted(0);
+//    }
   }
 
   private void updateGallery(List<Image> images) {
-    adapter.getImages().clear();
-    adapter.getImages().addAll(images);
+//    adapter.getImages().clear();
+//    adapter.getImages().addAll(images);
     adapter.notifyDataSetChanged();
+  }
+
+  @Override
+  public void onGalleryClick(String galleryId, View view) {
+
   }
 }
